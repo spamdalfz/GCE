70913b804b70885ecf025711a64bd12909fa1b9e
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2024-01-08 12:32:14 -0800

    added carousel

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,10 +1,19 @@
+import CarouselBox from "./carousel";
 import { Image } from "./image";
 import React from "react";
 
+
 export const Gallery = (props) => {
   return (
+
+      
     <div id="portfolio" className="text-center">
       <div className="container">
+      <div className="demo-carousel">
+        <CarouselBox>
+
+        </CarouselBox>
+      </div>
         <div className="section-title">
           <h2>Projects</h2>
           <h3>
